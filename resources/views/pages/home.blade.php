c596d23c48b8f4d6450ae3be157a09f2f415ea15
Aaron Warner <aaron-warner@Aarons-MBP.home>
2017-05-18 15:38:30 +0100

    added well to homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,17 @@
 
 @section('content')
 
+	<div class="col-xs-10 col-xs-offset-1">
+
+		<div class="well">
+
+			<h1>Welcome to Warner Web Design</h1>
+			<h3>Web Development in the West Midlands</h3>
+
+		</div>
+
+	</div>
+
 	<h1>YOYOYOYYOYOYOYOYO</h1>
 	<h2>I should see this heading</h2>
 	<h2>I should see this too</h2>
