5589b2aaff26cf827b4c459883d9f40bd78c4559
Aaron Warner <aaron-warner@Aarons-MBP.home>
2017-05-18 16:34:21 +0100

    homepage v1

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,19 +2,33 @@
 
 	<div class="row footer">
 		<div class="col-xs-10 col-xs-offset-1">
+			<div class="row">
 
-			<ul class="social-network social-circle unstyled inline">
-                <li>
-                	<a href="https://www.facebook.com/" class="icoFacebook" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-                </li>
-                <li>
-                	<a href="https://twitter.com/" class="icoTwitter" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a>
-                </li>
-                <li>
-                	<a href="https://www.linkedin.com/" class="icoLinkedin" title="Linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>
-                </li>
-            </ul>
+				<div class="col-md-4">
+					<h1>IMAGE WILL GO HERE</h1>
+				</div>
 
+				<div class="col-md-4">
+					<h1>MAYBE A CONTACT FORM HERE?</h1>
+				</div>
+
+				<div class="col-md-4">
+
+					<ul class="social-network social-circle unstyled inline">
+		                <li>
+		                	<a href="https://www.facebook.com/" class="icoFacebook" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+		                </li>
+		                <li>
+		                	<a href="https://twitter.com/" class="icoTwitter" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+		                </li>
+		                <li>
+		                	<a href="https://www.linkedin.com/" class="icoLinkedin" title="Linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>
+		                </li>
+		            </ul>
+
+				</div>
+
+            </div>
 		</div>
 	</div>
 
