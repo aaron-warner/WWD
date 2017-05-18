656f9e58827e1c2fe3fa7cdbac01277bc33350e2
Aaron Warner <aaron-warner@Aarons-MBP.home>
2017-05-18 16:33:39 +0100

    added message to homepage

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,32 @@
+<div class="container-fluid footer-container">
+
+	<div class="row footer">
+		<div class="col-xs-10 col-xs-offset-1">
+
+			<ul class="social-network social-circle unstyled inline">
+                <li>
+                	<a href="https://www.facebook.com/" class="icoFacebook" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                	<a href="https://twitter.com/" class="icoTwitter" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                	<a href="https://www.linkedin.com/" class="icoLinkedin" title="Linkedin" target="_blank"><i class="fa fa-linkedin"></i></a>
+                </li>
+            </ul>
+
+		</div>
+	</div>
+
+	<div class="row copyright">
+		<div class="col-xs-10 col-xs-offset-1">
+
+			<ul class="unstyled inline">
+				<li>Copyright &copy <?php echo date("Y"); ?>. All rights reserved</li>
+				<li class="pull-right">Developed by Aaron Warner</li>
+			</ul>
+
+		</div>
+	</div>
+
+</div>
